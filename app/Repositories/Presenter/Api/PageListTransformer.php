5598edeaa70927a41c8de6259d85a51d7c7c125a
wzj <wzj@8f4eb157-1f6c-9743-8fd1-3d82cfec6da5>
2018-06-22 08:42:27 +0000

    git-svn-id: https://39.108.144.9:8443/svn/feibucms@5 8f4eb157-1f6c-9743-8fd1-3d82cfec6da5

--- app/Repositories/Presenter/Api/PageListTransformer.php
+++ app/Repositories/Presenter/Api/PageListTransformer.php
@@ -16,7 +16,7 @@ class PageListTransformer extends TransformerAbstract
             'title' => $page->title,
             'image' => $page->image ? config('app.image_url').'/image/sm'.$page->image : '',
             //'images' => $page->images,
-            'description' => $page->description,
+            'description' => $page->description ? $page->description : get_substr(strip_tags($page->content),200),
             'category_id' => $page->category_id,
             'date' => $page->updated_at->format('Y-m-d'),
             'views_count' => $page->views_count,
